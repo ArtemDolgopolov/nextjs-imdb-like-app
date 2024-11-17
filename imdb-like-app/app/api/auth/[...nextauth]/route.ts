11691a4c92f2faa546c2cc0216e9f9aa1f2c96aa
ArtemDolgopolov <artem.dolgopolov94@gmail.com>
2024-11-17 23:00:54 +0500

    fix vercel deployment errors

--- imdb-like-app/app/api/auth/[...nextauth]/route.ts
+++ imdb-like-app/app/api/auth/[...nextauth]/route.ts
@@ -6,12 +6,12 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 const handler = NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialsProvider({
      name: 'Credentials',
